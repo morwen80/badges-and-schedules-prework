d1849ab10f49124a67ca903bd7d608a2466855c6
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-24 22:06:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(arr)
+  arr.collect {|attendee| "Hello, my name is #{attendee}."}
+end
+
+def assign_rooms(speaks)
+  messages = []
+  speaks.each_with_index { |speaker, room|
+  messages.push("Hello, #{speaker}! You'll be assigned to room #{room+1}!")}
+  
+  return messages
+end
+
+
+def printer(attendee)
+  return def badge_maker
+  
+end
